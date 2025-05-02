8084e6ac50856e1edc44b9861e6b8d38f17329a4
Aliatimis <aliatimi@vdi-stori-053.lancs.local>
2025-05-02 14:04:48 +0100

    renamed run.py

--- run_analysis.py
+++ run_analysis.py
@@ -3,5 +3,5 @@ from metal.estimators import *
 fasta_filepath = "gene_data/concatenated_seq_alignment.nex"
 
 metal_estimator(fasta_filepath, "best_tree_metal.tre")
-#metal_bootstrap_estimators(fasta_filepath, 1000)
+metal_bootstrap_estimators(fasta_filepath, 1000)
 multivariate_normal_bootstrap_estimators(fasta_filepath,1000,mutation_rate=1, sites_per_gene=100)
