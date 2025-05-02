a7d5db329636ae2c87b48e13bcc505a8089cd77f
Aliatimis <aliatimi@vdi-stori-053.lancs.local>
2025-05-02 11:45:28 +0100

    created setup and init files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="metal_covariance_matrix",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "biopython",
+        "tqdm",
+        "dendropy",
+        "pyvolve",
+    ],
+)
