6cd619e36bd67c010e317bbbc62d2f21c954f029
GeorgiosAliatimis <galiatimis@gmail.com>
2025-05-15 14:02:42 +0100

    plotting only through file or tree; types checked internally

--- utils/plot_tree.py
+++ utils/plot_tree.py
@@ -1,48 +1,32 @@
-import io 
 from Bio import Phylo 
 import matplotlib.pyplot as plt
 import dendropy
+from .treetools import convert_to_dendropy_tree, convert_to_phylo_tree, normalize_terminal_branch_lengths
 
-def plot_dendropy_tree(tree, **kwargs):
-    newick_tree = tree.as_string("newick")
-    handle = io.StringIO(newick_tree)
-    tree = Phylo.read(handle, "newick")
-    Phylo.draw(tree, **kwargs)
-
-def plot_tree_file(tree_filepath, schema, **kwargs):
-    tree = dendropy.Tree.get(path=tree_filepath,schema=schema)
-    plot_dendropy_tree(tree, **kwargs)
-
-
-def plot_tree_with_support(tree_filepath, axes = None):
-    tree = Phylo.read(tree_filepath, "newick")
-    if axes is None:
-        fig, axes = plt.subplots(figsize=(10, 10))
-    
-    # Plot the tree without showing it yet
-    Phylo.draw(tree, do_show=False, axes=axes)
-
-    depths = tree.depths()
-    
-    # Annotate the branches with support values (assuming support is stored in .confidence)
+def plot_tree(tree, ax = None, normalize = True):
+    if normalize:
+        tree = convert_to_dendropy_tree(tree)
+        normalize_terminal_branch_lengths(tree)
+    tree = convert_to_phylo_tree(tree)
+    do_show = ax is None
     for clade in tree.find_clades():
-        if hasattr(clade, 'confidence') and clade.confidence is not None:  # Check if there's a confidence score
-            # In Phylo.draw(), the horizontal position is given by the branch length (x)
-            # We can calculate the y-coordinate manually if needed, or use a fixed value for simplicity
-            if clade.branch_length is not None:
-                x = clade.branch_length
-            else:
-                x = 0  # If no branch length is available, use a default value (0 or other)
-            
-            # Use tree.depths() to calculate the vertical position (y) of the clade
-            y = depths.get(clade,0)  # Call the depths method to get the vertical position
-            
-            # Annotate with branch length and confidence value
-            axes.annotate(f'{clade.confidence:.2f}', xy=(x, y), 
-                        xytext=(x, y),  # Adjust the position of the text annotation
-                        arrowprops=dict(facecolor='black', arrowstyle="->"))
+        if not clade.is_terminal() and clade.confidence is not None:
+            clade.name = f"{100 * clade.confidence:.0f}%"        
+    Phylo.draw(tree, show_confidence = False, do_show = do_show, axes = ax)
+    if ax:
+        ax.set_ylabel("")             
+        ax.set_yticks([]) 
+        ax.spines['left'].set_visible(False)
+        ax.yaxis.set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)   
+
+def plot_tree_from_file(path, schema, **kwargs):
+    tree = Phylo.read(path, schema)
+    plot_tree(tree, **kwargs)
 
 def print_tree(tree, show_support=True, show_lengths=True):
+    tree = convert_to_dendropy_tree(tree)
     print_tree_helper(tree.seed_node, show_support = show_support, show_lengths=show_lengths)
     
 def print_tree_helper(node, indent="", is_last=True, show_support=True, show_lengths=True):
